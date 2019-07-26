0a2208bd84b49b6421f5a69bb4ec671a624fbc50
Sandris Artemjevs <sandris.artemjevs@wandoofinance.com>
2019-07-26 19:14:24 +0300

    fixed some mistakes

--- src/basics/02/10-calculator.js
+++ src/basics/02/10-calculator.js
@@ -11,5 +11,5 @@ function power() {}
 console.log(add(1, 2)); // Expected result: 3
 console.log(subtract(1, 2)); // Expected result: -1
 console.log(sum([1, 2, 3])); // Expected result: 6
-console.log(multiply([1, 2, 3])); // Expected result: 9
+console.log(multiply([1, 2, 3])); // Expected result: 6
 console.log(power(2, 3)); // Expected result: 8
--- src/basics/02/14-greatest-number.js
+++ src/basics/02/14-greatest-number.js
@@ -1,4 +1,4 @@
 function greatest(x, y) {}
 
-console.log(greatest(1, 2)); // Expected result: 1
+console.log(greatest(1, 2)); // Expected result: 2
 console.log(greatest(5, 2)); // Expected result: 5
